942708fc5b9d7c27ea4548064efa54f49bd396b7
alialaba <alialaba079@gmail.com>
2024-06-09 11:52:51 -0400

    chores(pages):

---
+++ src/Utils/setNavLinkAtr.ts
@@ -0,0 +1,16 @@
+// // Check if window and document are available
+// if(typeof window !== "undefined" && typeof document !== "undefined"){
+
+//     // Get all elements with the class 'nav__link'
+// const navLinks : NodeListOf <HTMLAnchorElement> = document.querySelectorAll(".nav__link");
+
+
+// //Iterate over each link
+// navLinks.forEach((link: HTMLAnchorElement)=>{
+//     // Check if the href of the link matches the current window location
+//     if(link.href == window.location.href){
+//         // Set the 'aria-current' attribute to 'page'
+//           link.setAttribute("aria-current", "page")
+//     }
+// })
+// }
